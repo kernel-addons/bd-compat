3781f2988d89e80cf4b5bd1b856cea30c2ef2932
Strencher <46447572+Strencher@users.noreply.github.com>
2022-02-19 14:40:19 +0100

    Fix webpack args

--- src/renderer/modules/webpack.ts
+++ src/renderer/modules/webpack.ts
@@ -35,7 +35,25 @@ class WebpackModule {
     get id() {return Symbol("pc-compat");}
 
     constructor() {
-        this.waitForGlobal.then(() => {
+        this.whenReady = this.waitForGlobal.then(() => new Promise(async onReady => {
+            const [Dispatcher, {ActionTypes} = {}, UserStore] = await this.findByProps(
+                ["dirtyDispatch"], ["API_HOST", "ActionTypes"], ["getCurrentUser", "_dispatchToken"],
+                {cache: false, bulk: true, wait: true, forever: true}
+            );
+
+            if (UserStore.getCurrentUser()) return onReady();
+            
+            const listener = function () {
+                Dispatcher.unsubscribe(ActionTypes.START_SESSION, listener);
+                Dispatcher.unsubscribe(ActionTypes.CONNECTION_OPEN, listener);
+                onReady();
+            };
+
+            Dispatcher.subscribe(ActionTypes.START_SESSION, listener);
+            Dispatcher.subscribe(ActionTypes.CONNECTION_OPEN, listener);
+        }));
+
+        this.whenReady.then(() => {
             let originalPush = window[this.chunkName].push;
 
             const handlePush = (chunk: any[]) => {
@@ -44,9 +62,10 @@ class WebpackModule {
                 for (const moduleId in modules) {
                     const originalModule = modules[moduleId];
 
-                    modules[moduleId] = (module, exports, require) => {
+                    modules[moduleId] = (...args) => {
+                        const [, exports] = args;
                         try {
-                            originalModule.call(originalModule, module, exports, require);
+                            originalModule.apply(originalModule, args);
 
                             const listeners = [...this.#listeners];
                             for (let i = 0; i < listeners.length; i++) {
@@ -83,24 +102,6 @@ class WebpackModule {
                 }
             });
         });
-
-        this.whenReady = this.waitForGlobal.then(() => new Promise(async onReady => {
-            const [Dispatcher, {ActionTypes} = {}, UserStore] = await this.findByProps(
-                ["dirtyDispatch"], ["API_HOST", "ActionTypes"], ["getCurrentUser", "_dispatchToken"],
-                {cache: false, bulk: true, wait: true, forever: true}
-            );
-
-            if (UserStore.getCurrentUser()) return onReady();
-            
-            const listener = function () {
-                Dispatcher.unsubscribe(ActionTypes.START_SESSION, listener);
-                Dispatcher.unsubscribe(ActionTypes.CONNECTION_OPEN, listener);
-                onReady();
-            };
-
-            Dispatcher.subscribe(ActionTypes.START_SESSION, listener);
-            Dispatcher.subscribe(ActionTypes.CONNECTION_OPEN, listener);
-        }));
     }
 
     addListener(listener: Function) {
